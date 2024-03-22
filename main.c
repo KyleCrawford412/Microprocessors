f406bd093dd20ac167325fa5733aa5a7272083ab
KyleCrawford412 <56799075+KyleCrawford412@users.noreply.github.com>
2024-03-22 16:27:16 -0400

    Add files via upload
    Statistical analysis of random number generation from seed.

--- main.c
+++ main.c
@@ -30,89 +30,124 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * --/COPYRIGHT--*/
 
+//Kyle Crawford
+//Assignment two
+//Fall 2020
+//11 AM section
 
-//include files
+//includes
 #include "driverlib.h"
 #include <stdio.h>
 #include <stdint.h>
-#include <HAL_UART_4746.h>
+
+//defines
+#define SEED 'C'
+#define LED2PORT GPIO_PORT_P1
+#define LED2PIN GPIO_PIN1
 
 //function prototypes
-bool checkPrime(uint16_t x);
+void myData(uint8_t, uint8_t*);
 
-
-//checkPrime
-//Inputs: usigned 16 bit
-//outputs: bool
-//returns true if number is a prime, false if number is not a prime
-bool checkPrime(uint16_t x){
-
-    //variables
-    uint16_t i;
-
-    //special cases
-    if((x == 0) || (x == 1)){
-        return false;
-    }
-    else if((x == 2) || (x == 3)){
-        return true;
-    }
-    else{
-        for(i = 4; i<= x; i++){
-            if(x%i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-//main
 void main (void){
 
-    //define variables
-    uint8_t i = 0;
-    uint16_t ii, maxNum = 0;
-    volatile static uint16_t numPrimes = 0;
-    char buffer[100];
+    //variables
+    uint8_t count0 = 0, count1 = 0, count2 = 0, count3 = 0, count4 = 0, u8bData, avg0, avg1, avg2, avg3, avg4;
+    uint16_t sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+    uint16_t v0 = 0, v1 = 0;
+    uint32_t v2 = 0, v3 = 0, v4 = 0;
+    uint32_t temp = 0;
 
-    //hold WDT
+
+    //bins
+    uint8_t bin0[256], bin1[256], bin2[256], bin3[256], bin4[256];
+
+    //stop WDT
     WDT_A_hold(WDT_A_BASE);
 
-    UART_initGPIO();
-    UART_init();
-    _delay_cycles(500000);
+    //define I/O
+    GPIO_setAsOutputPin(LED2PORT, LED2PIN);
+    GPIO_setOutputLowOnPin(LED2PORT, LED2PIN);
+    PMM_unlockLPM5();
 
-    //print
-    sprintf(buffer, "Kyle Crawford \r\n");
-    UART_transmitString(buffer);
-    UART_transmitString("Fall 2020\r\n");
+    //initialize myData
+    myData(SEED, &u8bData);
 
-
-    //loop i from 0 to 10
-    for(i = 0; i <= 10; i++){
-        maxNum = 500*(1+2*i);
-        //check all the numbers
-        for(ii = 0; ii <= maxNum; ii++){
-            //check for prime number
-            if(checkPrime(ii)){
-                numPrimes++;
-            }
+    //get random numbers into array
+    myData(1, &u8bData);
+    while(u8bData != 0){
+       //range 1 to 49
+        if(u8bData < 50){
+            bin0[count0] = u8bData;
+            count0++;
+            sum0 += u8bData;
+            temp = u8bData * u8bData;
+            v0 += temp;
         }
-        //print results and reset count
-        sprintf(buffer, "Number of primes: %d\r\n", numPrimes);
-        UART_transmitString(buffer);
-        numPrimes = 0;
+        //range 50 to 99
+        else if((u8bData >= 50) && (u8bData < 100)){
+            bin1[count1] = u8bData;
+            count1++;
+            sum1 += u8bData;
+            temp = u8bData * u8bData;
+            v1 += temp;
+        }
+        //range 100 to 149
+        else if((u8bData >= 100) && (u8bData < 150)){
+            bin2[count2] = u8bData;
+            count2++;
+            sum2 += u8bData;
+            temp = u8bData * u8bData;
+            v2 += temp;
+        }
+        //range 150 to 199
+        else if((u8bData >= 150) && (u8bData < 200)){
+            bin3[count3] = u8bData;
+            count3++;
+            sum3 += u8bData;
+            temp = u8bData * u8bData;
+            v3 += temp;
+        }
+        //range 200 to 255
+        else if((u8bData >= 200) && (u8bData < 256)){
+            bin4[count4] = u8bData;
+            count4++;
+            sum4 += u8bData;
+            temp = u8bData * u8bData;
+            v4 += temp;
+        }
     }
 
+    //calculate averages
+    avg0 = sum0 / count0;
+    avg1 = sum1 / count1;
+    avg2 = sum2 / count2;
+    avg3 = sum3 / count3;
+    avg4 = sum4 / count4;
+
+    //calculate variances
+    v0 = (v0 / count0) - (avg0 * avg0);
+    v1 = (v1 / count1) - (avg1 * avg1);
+    v2 = (v2 / count2) - (avg2 * avg2);
+    v3 = (v3 / count3) - (avg3 * avg3);
+    v4 = (v4 / count4) - (avg4 * avg4);
+
+    //print out results
+    puts("Kyle Crawford");
+    puts("Fall 2020");
+    puts("CHW#3");
+    puts("My seed number is 0x43");
+    puts("N, #, Avg, Variance");
+
+    printf("0: %d\n", vSum);
+    printf("1: %d\n", vAvg);
+    printf("2: %d\n", vMax);
+    printf("3: %d\n", vMin);
+    printf("4: %d\n", vPos);
 
 
-    sprintf(buffer, "Number of primes: %d\r\n", numPrimes);
-    UART_transmitString(buffer);
-
+    GPIO_setOutputHighOnPin(LED2PORT, LED2PIN);
 
     //spin
     while(1);
-
 }
 
